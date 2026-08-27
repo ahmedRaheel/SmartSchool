08db4830930878d31177cb68171e255020d2c319
Ahmed Raheel <raaheelahmed@outlook.com>
2026-08-27 15:29:04 +0500

    added branch id in identity

---
+++ src/Modules/Identity/Persistence/Migrations/PostgreSql/AspNetIdentity/20260827102838_AddBranchId.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartSchool.Modules.Identity.Persistence.Migrations.PostgreSql.AspNetIdentity
+{
+    /// <inheritdoc />
+    public partial class AddBranchId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BranchId",
+                schema: "identity",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MustChangePassword",
+                schema: "identity",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "PasswordChangedAt",
+                schema: "identity",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SchoolId",
+                schema: "identity",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BranchId",
+                schema: "identity",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "MustChangePassword",
+                schema: "identity",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordChangedAt",
+                schema: "identity",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SchoolId",
+                schema: "identity",
+                table: "Users");
+        }
+    }
+}
