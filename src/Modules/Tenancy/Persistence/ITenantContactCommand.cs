936cfd244582f91c26eb6a34a453add92b9b4031
Ahmed Raheel <raaheelahmed@outlook.com>
2026-08-30 20:00:58 +0500

    clean up code

--- src/Modules/Tenancy/Persistence/ITenantContactCommand.cs
+++
@@ -1,3 +0,0 @@
-using SmartSchool.Modules.Tenancy.Models;
-namespace SmartSchool.Modules.Tenancy.Persistence;
-public interface ITenantContactCommand { Task AddAsync(TenantContactEntity entity, CancellationToken cancellationToken); }
