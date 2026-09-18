4efd394446df6700569510a3d4d1478b51edd0a0
Ahmed Raheel <raaheelahmed@outlook.com>
2026-09-18 15:10:43 +0500

    fix

--- src/BuildingBlocks/SmartSchool.Infrastructure/Persistence/ModuleDbContextOptionsConfigurator.cs
+++ src/BuildingBlocks/SmartSchool.Infrastructure/Persistence/ModuleDbContextOptionsConfigurator.cs
@@ -128,10 +128,10 @@ public sealed class ModuleDbContextOptionsConfigurator(
         where TContext : DbContext
     {
         const string prefix = "__EFMigrationsHistory_";
-        const int PostgreSqlIdentifierLimit = 63;
+        const int postgreSqlIdentifierLimit = 63;
 
         var contextName = typeof(TContext).Name;
-        var availableLength = PostgreSqlIdentifierLimit - prefix.Length;
+        var availableLength = postgreSqlIdentifierLimit - prefix.Length;
 
         if (contextName.Length > availableLength)
         {
